f161994deb4156925152126d1089b51e6006c09a
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-30 23:10:47 +0200

    [UPD] bikes array done

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Snack 1:
+
+const bikes = [
+    {
+        name: 'BMX',
+        weight: 15,
+    },
+    {
+        name: 'Mountain bike',
+        weight: 10,
+    },
+    {
+        name: 'City bike',
+        weight: 12,
+    },
+    {
+        name: 'racing bike',
+        weight: 8,
+    },
+]
