3e44e06ee3fa93eb0205896e028baab277211bd0
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-30 23:34:15 +0200

    [UPD] arrey ex 2 done

---
+++ ex2/js/script.js
@@ -0,0 +1,24 @@
+// Snack 2:
+
+const teams = [
+    {
+        name: 'Juventus',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Inter',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Napoli',
+        points: 0,
+        fouls: 0,
+    },
+    {
+        name: 'Atalanta',
+        points: 0,
+        fouls: 0,
+    },
+]
